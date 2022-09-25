765ef3a98918b03359fd06f465365c45e518f735
hilalozcanyavuz <113976776+hilalozcanyavuz@users.noreply.github.com>
2022-09-26 01:49:40 +0300

    Add files via upload

---
+++ classesWithAttribute/src/classesWithAttribute/Main.java
@@ -0,0 +1,36 @@
+package classesWithAttribute;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Product product= new Product(1, "Laptop", "Asus Laptop", 3000, 2, "Siyah");
+		/* product.name= "Laptop";
+		product.id=1;
+		product.description= "Asus Laptop";
+		product.price=5000;
+		product.stockAmount=3; 
+		product.renk=""; */
+		
+		/* product.setName("Laptop");
+		product.setId(1);
+		product.setDescription("Asus Laptop");
+		product.setPrice(5000);
+		product.setStockAmount(3);
+		
+		
+		System.out.println(product.getName());
+		System.out.println(product.getId());
+		System.out.println(product.getDescription());
+		System.out.println(product.getPrice());
+		System.out.println(product.getStockAmount()); */
+		
+		ProductManager productManager = new ProductManager ();
+		
+		productManager.Add(product);
+		System.out.println(product.getKod());
+		
+
+	}
+
+}
